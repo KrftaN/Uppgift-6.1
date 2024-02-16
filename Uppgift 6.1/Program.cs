4f74fc47a8156835cd9ef9243b99496570c33ce4
Admin <Admin@TEB12703>
2024-02-16 10:51:12 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Uppgift_6._1
+{
+    class Program
+    {
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv tre tal separerade med kommatecken [1, 2, 3]");
+            int[] numbers = Console.ReadLine().Split(',').Select(x => int.Parse(x.Trim())).ToArray();
+
+            int sum = Addera(numbers[0], numbers[1], numbers[2]);
+            Console.WriteLine($"Summan av talen {numbers[0]}, {numbers[1]}, {numbers[2]} är: {sum}");
+        }
+    }
+}
